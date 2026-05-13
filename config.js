29117baef70f2af3c8019dc246a5b3e79289aada
ediallo <dialloe1999@gmail.com>
2026-05-13 14:46:17 +0000

    une liaison avec le backend

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const API_URL = "http://127.0.0.1:5000/api";
-
+// Remplaçons http://127.0.0.1:5000/api par le lien Render
+const API_URL = "https://red-product-backend-jyol.onrender.com/api";
 function logout() {
     localStorage.removeItem('token');
     localStorage.removeItem('user');
